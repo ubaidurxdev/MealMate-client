899f8db5e12a4c5f32e4eebe2bee5b5ddc29e586
noob-ubaid <ubaidurrahman661@gmail.com>
2026-02-17 22:04:11 +0600

    add navbvar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Navbar5 } from "@/components/navbar5";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Navbar5/>
         {children}
       </body>
     </html>
