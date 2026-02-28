4006d93e8d91c0fc20a7a0de95a92716323ad96c
noob-ubaid <ubaidurrahman661@gmail.com>
2026-03-01 00:23:14 +0600

    add get input field data

--- app/(commonLayout)/register/page.tsx
+++ app/(commonLayout)/register/page.tsx
@@ -12,17 +12,21 @@ import { authClient } from "@/lib/auth-client";
 
 export default function RegisterPage() {
   const [showPassword, setShowPassword] = useState(false);
-  const handleGoogleLogin = async () => {
-    try {
-      const data = authClient.signIn.social({
-        provider: "google",
-        callbackURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
-      });
 
-      console.log("Success:", data);
-    } catch (error) {
-      console.error("Google Login Error:", error);
-    }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, email, password);
+  };
+
+  const handleGoogleLogin = async () => {
+    await authClient.signIn.social({
+      provider: "google",
+      callbackURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+    });
   };
 
   return (
@@ -36,70 +40,78 @@ export default function RegisterPage() {
             </h1>
           </div>
 
-          {/* Name Field */}
-          <div className="space-y-2">
-            <label className="text-sm font-medium">Full Name</label>
-            <div className="relative">
-              <User className="absolute left-3 top-5 h-4 w-4 text-muted-foreground" />
-              <Input
-                type="text"
-                placeholder="John Doe"
-                className="pl-10 mt-2"
-              />
+          <form onSubmit={handleSubmit}>
+            {/* Name Field */}
+            <div className="space-y-2">
+              <label className="text-sm font-medium">Full Name</label>
+              <div className="relative">
+                <User className="absolute left-3 top-5 h-4 w-4 text-muted-foreground" />
+                <Input
+                  type="text"
+                  name="name"
+                  placeholder="John Doe"
+                  className="pl-10 mt-2"
+                />
+              </div>
             </div>
-          </div>
 
-          {/* Email Field */}
-          <div className="space-y-2">
-            <label className="text-sm font-medium">Email</label>
-            <div className="relative">
-              <Mail className="absolute left-3 top-5 h-4 w-4 text-muted-foreground" />
-              <Input
-                type="email"
-                placeholder="you@example.com"
-                className="pl-10 mt-2"
-              />
+            {/* Email Field */}
+            <div className="space-y-2">
+              <label className="text-sm font-medium">Email</label>
+              <div className="relative">
+                <Mail className="absolute left-3 top-5 h-4 w-4 text-muted-foreground" />
+                <Input
+                  type="email"
+                  name="email"
+                  placeholder="you@example.com"
+                  className="pl-10 mt-2"
+                />
+              </div>
             </div>
-          </div>
 
-          {/* Password Field */}
-          <div className="space-y-2">
-            <label className="text-sm font-medium">Password</label>
-            <div className="relative">
-              <Lock className="absolute left-3 top-5 h-4 w-4 text-muted-foreground" />
-              <Input
-                type={showPassword ? "text" : "password"}
-                placeholder="Create a password"
-                className="pl-10 pr-10 mt-2"
-              />
-              <button
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
-                className="absolute right-3 top-5 text-muted-foreground"
+            {/* Password Field */}
+            <div className="space-y-2">
+              <label className="text-sm font-medium">Password</label>
+              <div className="relative">
+                <Lock className="absolute left-3 top-5 h-4 w-4 text-muted-foreground" />
+                <Input
+                  name="password"
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Create a password"
+                  className="pl-10 pr-10 mt-2"
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-3 top-5 text-muted-foreground"
+                >
+                  {showPassword ? (
+                    <EyeOff className="h-4 w-4" />
+                  ) : (
+                    <Eye className="h-4 w-4" />
+                  )}
+                </button>
+              </div>
+            </div>
+
+            {/* User role Field */}
+            <div className="space-y-2">
+              <label className="text-sm font-medium">Sign up as</label>
+
+              <select
+                name="role"
+                className=" block  p-2 file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm "
               >
-                {showPassword ? (
-                  <EyeOff className="h-4 w-4" />
-                ) : (
-                  <Eye className="h-4 w-4" />
-                )}
-              </button>
+                <option value="customer">Customer</option>
+                <option value="provider">Provider</option>
+              </select>
             </div>
-          </div>
 
-          {/* Confirm Password Field */}
-          <div className="space-y-2">
-            <label className="text-sm font-medium">Sign up as</label>
-
-            <select className=" block  p-2 file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm ">
-              <option value="customer">Customer</option>
-              <option value="provider">Provider</option>
-            </select>
-          </div>
-
-          {/* Register Button */}
-          <Button className="w-full bg-black text-white hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200 transition">
-            Create Account
-          </Button>
+            {/* Register Button */}
+            <Button className="w-full bg-black text-white hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200 transition">
+              Create Account
+            </Button>
+          </form>
 
           {/* Divider */}
           <div className="flex items-center gap-4">
@@ -111,7 +123,7 @@ export default function RegisterPage() {
           {/* Social Signup */}
           <Button
             variant="outline"
-            onClick={() => handleGoogleLogin()}
+            onClick={handleGoogleLogin}
             className="w-full border-gray-300 dark:border-gray-700"
           >
             <Google />
