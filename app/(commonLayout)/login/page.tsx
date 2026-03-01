38b23d704a1d1ce0fb037281e941c7cba1516434
noob-ubaid <ubaidurrahman661@gmail.com>
2026-03-01 13:00:46 +0600

    add login functionality using email and password

--- app/(commonLayout)/login/page.tsx
+++ app/(commonLayout)/login/page.tsx
@@ -8,13 +8,32 @@ import { Input } from "@/components/ui/input";
 import { Card, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import Google from "@/components/svg/Google";
+import { authClient } from "@/lib/auth-client";
+import { toast } from "sonner";
 
 export default function LoginPage() {
   const [showPassword, setShowPassword] = useState(false);
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const { data, error  } = await authClient.signIn.email({
+      email: email,
+      password: password,
+      rememberMe: true,
+      callbackURL: "http://localhost:3000",
+    });
+    console.log(data);
+    toast.success("Successfully logged in");
+    if (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="h-[calc(100vh-68px)] flex items-center justify-center  px-4 ">
       <Card className="w-full max-w-md border border-gray-200 dark:border-gray-800">
-        <CardContent className="p-8 space-y-6">
+        <CardContent className="p-6 space-y-6">
           {/* Logo + Title */}
           <div className="text-center space-y-2">
             <h1 className="text-3xl font-bold tracking-tight pb-4 border-b border-gray-200 dark:border-gray-800 text-black dark:text-white">
@@ -22,47 +41,50 @@ export default function LoginPage() {
             </h1>
           </div>
 
-          {/* Email Field */}
-          <div className="space-y-2">
-            <label className="text-sm font-medium">Email</label>
-            <div className="relative">
-              <Mail className="absolute left-3 top-5 h-4 w-4 text-muted-foreground" />
-              <Input
-                type="email"
-                placeholder="you@example.com"
-                className="pl-10 mt-2"
-              />
+          <form onSubmit={handleSubmit}>
+            {/* Email Field */}
+            <div className="space-y-2">
+              <label className="text-sm font-medium">Email</label>
+              <div className="relative">
+                <Mail className="absolute left-3 top-5 h-4 w-4 text-muted-foreground" />
+                <Input
+                  type="email"
+                  name="email"
+                  placeholder="you@example.com"
+                  className="pl-10 mt-2"
+                />
+              </div>
             </div>
-          </div>
 
-          {/* Password Field */}
-          <div className="space-y-2">
-            <label className="text-sm font-medium">Password</label>
-            <div className="relative">
-              <Lock className="absolute left-3 top-5 h-4 w-4 text-muted-foreground" />
-              <Input
-                type={showPassword ? "text" : "password"}
-                placeholder="Enter your password"
-                className="pl-10 pr-10 mt-2"
-              />
-              <button
-                type="button"
-                onClick={() => setShowPassword(!showPassword)}
-                className="absolute right-3 top-5 text-muted-foreground"
-              >
-                {showPassword ? (
-                  <EyeOff className="h-4 w-4" />
-                ) : (
-                  <Eye className="h-4 w-4" />
-                )}
-              </button>
+            {/* Password Field */}
+            <div className="space-y-2 mt-2">
+              <label className="text-sm font-medium">Password</label>
+              <div className="relative">
+                <Lock className="absolute left-3 top-5 h-4 w-4 text-muted-foreground" />
+                <Input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="Enter your password"
+                  name="password"
+                  className="pl-10 pr-10 mt-2"
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="absolute right-3 top-5 text-muted-foreground"
+                >
+                  {showPassword ? (
+                    <EyeOff className="h-4 w-4" />
+                  ) : (
+                    <Eye className="h-4 w-4" />
+                  )}
+                </button>
+              </div>
             </div>
-          </div>
-
-          {/* Login Button */}
-          <Button className="w-full bg-black text-white hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200 transition">
-            Sign In
-          </Button>
+            {/* Login Button */}
+            <Button className="w-full mt-5 bg-black text-white hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200 transition">
+              Sign In
+            </Button>
+          </form>
 
           {/* Divider */}
           <div className="flex items-center gap-4">
